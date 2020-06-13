53fe36a118d28760143ea4a2116c7037afaa2cf8
Hossein Sadeghi <hse1984@gmail.com>
2020-06-12 23:10:53 -0700

    bug fix, change IMAGE_FOLDER value

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['IMAGE_FOLDER'] = UPLOAD_FOLDER
+app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
 app.config['SECRET_KEY'] = '2b55241464af362a104880e46b36d2b6'
 
 
